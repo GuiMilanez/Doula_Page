7f82e207531f20b44ae767f3c3e8a07fab2d4899
Gui <glmilanez@gmail.com>
2019-04-07 12:11:03 -0400

    rebuild links

---
+++ app/controllers/references_controller.rb
@@ -0,0 +1,7 @@
+class 
+  ReferencesController < ApplicationController
+  def index
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   resources :posts
+  get '/services', to: 'services#index'
+  get '/references', to: 'references#index'
+  get '/contact', to: 'contact#index'
 
   root 'posts#index'
 end
